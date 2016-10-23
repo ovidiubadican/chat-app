aadee9ea3151fd9a529eb6b0ff520c79239fe172
Ovidiu Badican <badicanovidiu@gmail.com>
2016-10-23 07:00:19 +0000

    Encryption

--- client.py
+++ client.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-import socket, select, sys
-from config import *
 
+import socket, sys
+from config import PORT, RECV_BUFFER
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+# connect to server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 socket_list = [s]
@@ -12,12 +17,31 @@ except:
     print("Unable to connect!")
     sys.exit()
 
-print("Connected to the chat server. Start sending messages!")
-print("Sending a test message...")
+print("Connected to the chat server.")
+print("Sending an encrypted test message...")
 
-data = "Test message."
+# plaintext message
+data = "Test message.".encode('utf-8')
+
+# generate our keys for encryption
+random_generator = Random.new().read
+key = RSA.generate(1024, random_generator)
+
+# extract the public key and send it to the server
+public_key = key.publickey()
+s.send(public_key)
+s.recv(RECV_BUFFER)
+
+
+# generate a hash from the plaintext message and sign it
+hash = SHA256.new(data).digest()
+signature = key.sign(hash, random_generator)
+
+# encrypt the data to be sent
+enc_data = public_key.encrypt(data,random_generator)
+
 try:
-    s.send(data.encode('utf-8'))
+    s.send(enc_data)
 except:
     print("Sending message failed!")
     sys.exit()
