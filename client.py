f451d41d890de170f99e0899d5bf3d6f466d319c
Ovidiu Badican <badicanovidiu@gmail.com>
2016-10-26 17:41:43 +0300

    Updating

--- client.py
+++ client.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-import socket, select, sys
-from config import PORT
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA
-from Crypto import Random
-"""
-=======
 
-import socket, sys
 from config import PORT, RECV_BUFFER, SERVER
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto import Random
+import socket, select, sys
 import pickle
 
 # connect to server
->>>>>>> a782797b19f24b7d3956fdcbbb89e741ddf03db9
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_list = [s]
 
@@ -27,57 +18,35 @@ except:
     sys.exit()
 
 print("Connected to the chat server.")
-<<<<<<< HEAD
-print("Sending a test message...")
-"""
-# the data we want to send
-data = "Test message."
+print("Start sending messages...")
 
-# generate pair or RSA keys
-key = RSA.generate(2048, random_generator)
+data = ""
 
-# create a SHA256 hash from the plaintext data
-hash = SHA256.new(data).digest()
-print(hash)
-"""
-try:
-    s.send(data.encode('utf-8'))
-except:
-    print("Sending message failed!")
-    sys.exit()
-"""
-=======
+while data != ":q!":
 
-# plaintext message
-data = "Super secret message".encode('utf-8')
+    data = input("Me: >")
+    # generate our keys for encryption
+    random_generator = Random.new().read
+    key = RSA.generate(1024, random_generator)
 
-# generate our keys for encryption
-random_generator = Random.new().read
-key = RSA.generate(1024, random_generator)
+    # extract the public key and send it to the server
+    public_key = key.publickey()
 
-# extract the public key and send it to the server
-public_key = key.publickey()
+    # serialize key to send across network
+    to_send = pickle.dumps(public_key)
+    s.send(to_send)
 
-# serialize key to send across network
-to_send = pickle.dumps(public_key)
-s.send(to_send)
-print("Client sent key to server...")
+    # receive key from server
+    server_key_string = s.recv(RECV_BUFFER)
+    server_key = pickle.loads(server_key_string)
 
-# receive key from server
-server_key_string = s.recv(RECV_BUFFER)
-server_key = pickle.loads(server_key_string)
-if server_key:
-    print("Client received server key")
+    # enrypt the data, hash it, sign it, and send it as a tuple
+    hash = SHA256.new(data).digest()
+    enc_data = server_key.encrypt(data, random_generator)
+    signature = key.sign(hash, random_generator)
+    data_tuple = (signature, hash, enc_data)
 
-print("Sending an encrypted test message...")
+    data_to_send = pickle.dumps(data_tuple)
+    s.send(data_to_send)
 
-hash = SHA256.new(data).digest()
-enc_data = server_key.encrypt(data, random_generator)
-signature = key.sign(hash, random_generator)
-data_tuple = (signature, hash, enc_data)
-
-data_to_send = pickle.dumps(data_tuple)
-s.send(data_to_send)
-
-print("Encrypted message sent!")
->>>>>>> a782797b19f24b7d3956fdcbbb89e741ddf03db9
+s.close()
--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import socket, select, sys
 from config import PORT, RECV_BUFFER
-<<<<<<< HEAD
-=======
+
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto import Random
 import pickle
->>>>>>> a782797b19f24b7d3956fdcbbb89e741ddf03db9
 
 connection_list = []
 
@@ -30,7 +28,7 @@ while True:
             clientsocket, addr = serversocket.accept()
             connection_list.append(clientsocket)
             print("Client %s:%s connected!" % addr)
-            
+
             # generate RSA keys
             random_generator = Random.new().read
             key = RSA.generate(1024, random_generator)
@@ -41,36 +39,25 @@ while True:
             # serialize key for sending across network
             to_send = pickle.dumps(public_key)
             clientsocket.send(to_send)
-            print("Server sent key...")
 
             # receive the key from client
             client_key_string = clientsocket.recv(RECV_BUFFER)
             client_key = pickle.loads(client_key_string)
 
-            if client_key:
-                print("Server received client key")
-
             cipher_string = clientsocket.recv(RECV_BUFFER)
             cipher = pickle.loads(cipher_string)
 
-            if cipher:
-
-                print("The received cypher is:", cipher)
-                print("Now verifying if message is authentic...")
-
-                signature = cipher[0]
-                hash = cipher[1]
-                client_message = key.decrypt(cipher[2]).decode('utf-8')
-                authentic = client_key.verify(hash, signature)
-
-                if authentic:
-                    print("The message is authentic!")
-                    print("The message is:", client_message)
-                else:
-                    print("The message is NOT authentic!!")
-                 
-            serversocket.close()
-            sys.exit()
+            signature = cipher[0]
+            hash = cipher[1]
+            client_message = key.decrypt(cipher[2]).decode('utf-8')
+            authentic = client_key.verify(hash, signature)
 
         else:
-            pass
+            data = sock.recv(RECV_BUFFER)
+
+            if data:
+
+
+
+serversocket.close()
+sys.exit()
