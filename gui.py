9c7b6818d189bf102bb9b3007edf2d94f92f9ef3
Ovidiu Badican <badicanovidiu@gmail.com>
2016-11-29 16:09:48 +0200

    Update

--- gui.py
+++ gui.py
@@ -5,8 +5,9 @@
 PyQt5 tutorial
 """
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QMessageBox
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
 
 class Window(QWidget):
 
@@ -15,10 +16,31 @@ class Window(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 300, 220)
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a button')
+        btn.resize(btn.sizeHint())
+        btn.move(50,50)
+
+        qbtn = QPushButton('Quit', self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 80)
+
+        self.setGeometry(1500, 120, 350, 800)
         self.setWindowTitle('ChatApp')
         self.setWindowIcon(QIcon('icon.jpg'))
         self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure you want to quit?", QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
